1923fbc0f2905d92fb82750b2b3fd59b647d7909
antonn01 <antonnurfendi01@gmail.com>
2022-10-10 16:12:32 +0700

    Pertemuan 4

---
+++ Tugas/Pertemuan3/tugas_3.py
@@ -0,0 +1,10 @@
+#A+B
+matriksA = 2,3,4,5
+matriksB = 9,8,7,6
+
+a11 = matriksA[0] + matriksB[0]
+a12 = matriksA[1] + matriksB[1]
+a21 = matriksA[2] + matriksB[2]
+a22 = matriksA[3] + matriksB[3]
+
+print("[",a11,a12,"] \n" "[",a21,a22,"]")
